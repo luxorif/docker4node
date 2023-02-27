8c57b6bae647953db6ea28a9a73b3365c46879db
luxorif <76787236+luxorif@users.noreply.github.com>
2023-02-27 08:18:49 +0100

    Update docker4node.sh

--- docker4node.sh
+++ docker4node.sh
@@ -2,7 +2,6 @@
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-yes Y
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
 sudo apt install docker-ce
